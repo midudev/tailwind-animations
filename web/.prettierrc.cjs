e671e4d3b6dbd49119a399b0e3b23f8fb2f831e0
jamerrq <jamerrq@gmail.com>
2024-02-06 15:09:23 -0500

    🔧 feat: eslint, prettier, and tsconfig configuration for web

---
+++ web/.prettierrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  ...require('prettier-config-standard'),
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ]
+}
