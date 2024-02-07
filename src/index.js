c634962c413164933354b2a7dedc8862c90c67ad
Miguel Ángel Durán <miduga@gmail.com>
2024-02-07 09:35:42 +0100

    Corrección de errores en el archivo index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const pluginCreator = api => {
   const dynamicUtils = {
     'animate-delay': { css: 'animation-delay', values: theme('animationDelay') },
     'animate-duration': { css: 'animation-duration', values: theme('animationDuration') },
-    'animate-iteration-count': { css: 'animation-iteration-count', values: theme('animationIterationCount') }
-    'animate-fill-mode': { css: 'animation-fill-mode', values: theme('animationFillMode') },
+    'animate-iteration-count': { css: 'animation-iteration-count', values: theme('animationIterationCount') },
+    'animate-fill-mode': { css: 'animation-fill-mode', values: theme('animationFillMode') }
   }
 
   Object.entries(dynamicUtils).forEach(([name, { css, values }]) => {
