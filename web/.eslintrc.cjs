d358fcfb7e088fb116016a28fd865a5884e7a98b
Ricardo <soyricardodev@proton.me>
2024-02-07 16:00:02 +0000

    add prettier config to eslint and tailwind plugin

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,6 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['plugin:astro/recommended'],
+  extends: ['plugin:astro/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
