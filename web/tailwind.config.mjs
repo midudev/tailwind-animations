8de4d56412dad12b97dc238d3dee0223a648424c
ivanosquis10 <ivanosquis10.12@gmail.com>
2024-02-09 11:27:57 -0400

    add: configure in tailwindcss.config for the dark mode

--- web/tailwind.config.mjs
+++ web/tailwind.config.mjs
@@ -7,6 +7,7 @@ const safelist = Object.keys(animation).map((key) => `animate-${key}`)
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {}
