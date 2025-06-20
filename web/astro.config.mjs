4b1e2a4ab547dcdf6c3320ac914ac4472cd858b8
midudev <miduga@gmail.com>
2025-06-20 10:44:47 +0200

    chore: migrate from @astrojs/tailwind to @tailwindcss/vite and update dependencies

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,7 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwindcss()]
+})
