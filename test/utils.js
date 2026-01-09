df6fdd1ed17e7e771bf2f4bbd27c817c2c34b137
midudev <miduga@gmail.com>
2026-01-09 20:15:34 +0100

    refactor: update Tailwind CSS import and plugin processing in utils.js

--- test/utils.js
+++ test/utils.js
@@ -1,22 +1,21 @@
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/postcss'
 import postcss from 'postcss'
 import minify from '@csstools/postcss-minify'
-import animationsPlugin from '../src/index.js'
+import { join } from 'path'
 
-const TAILWIND_BASE = '@tailwind utilities;'
+const TAILWIND_BASE = '@import "tailwindcss";'
+const PLUGIN_IMPORT = `@import "${join(process.cwd(), 'src/index.css')}";`
 
 export function generatePluginCSS (options = {}) {
   const { inline = '', content = '' } = options
 
   return postcss([
-    minify(),
-    tailwindcss({
-      plugins: [animationsPlugin],
-      content: [{ raw: content }]
-    })
+    tailwindcss(),
+    minify()
   ])
-    .process(`${TAILWIND_BASE} ${inline}`, {
-      from: undefined
+    .process(`${TAILWIND_BASE}\n${PLUGIN_IMPORT}\n${inline}`, {
+      from: join(process.cwd(), 'test/index.test.js'),
+      content: [{ raw: content, extension: 'html' }]
     })
     .then(result => result.css)
 }
